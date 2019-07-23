708ad888110fbd1dbe039aa7c0e06132b42bc3f0
Aleksandr Pertsev <aleksandr.pertsev@gmail.com>
2019-07-23 14:38:23 +0300

    fix

--- OnionAPI/Global.asax.cs
+++ OnionAPI/Global.asax.cs
@@ -18,7 +18,7 @@ namespace OnionAPI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            UnityConfig.RegisterComponents();
+            UnityConfig.RegisterComponents(); 
         }
     }
 }
